602ccce3705e4ee08ba761ef0d951c744f632199
Станислав Пусеп <stas@sysd.org>
2014-07-10 08:06:47 +0000

    Import of SYP/App-rainbarf-1.2 from CPAN.
    gitpan-cpan-distribution: App-rainbarf
    gitpan-cpan-version:      1.2
    gitpan-cpan-path:         SYP/App-rainbarf-1.2.tar.gz
    gitpan-cpan-author:       SYP
    gitpan-cpan-maturity:     released

--- lib/App/rainbarf.pm
+++ lib/App/rainbarf.pm
@@ -3,7 +3,7 @@ package App::rainbarf;
 use strict;
 use warnings;
 
-our $VERSION = q(1.1);
+our $VERSION = q(1.2);
 
 1;
 
@@ -16,7 +16,7 @@ rainbarf - CPU/RAM/battery stats chart bar for tmux (and GNU screen)
 
 =head1 VERSION
 
-version 1.1
+version 1.2
 
 =head1 DESCRIPTION
 
@@ -29,7 +29,7 @@ Stanislaw Pusep <stas@sysd.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2013 by Stanislaw Pusep <stas@sysd.org>.
+This software is copyright (c) 2014 by Stanislaw Pusep <stas@sysd.org>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
